faf84047b270d39e9663428ccd1423c8f4ffcadc
mgeovany <marlongeo1999@gmail.com>
2024-03-13 17:46:11 -0600

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+import $ from 'dom7';
+import Framework7 from './framework7-custom.js';
+
+// Import F7 Styles
+import '../css/framework7-custom.less';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.less';
+
+
+// Import Routes
+import routes from './routes.js';
+// Import Store
+import store from './store.js';
+
+// Import main app component
+import App from '../app.f7';
+
+
+var app = new Framework7({
+  name: 'Rare Beauty App', // App name
+  theme: 'auto', // Automatic theme detection
+  colors: {
+    primary: '#f2e2d3',
+  },
+  darkMode: true,
+  el: '#app', // App root element
+  component: App, // App main component
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+});
