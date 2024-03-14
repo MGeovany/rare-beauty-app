9f8706ced1482e95f5fb8c22bf03ead6b4784109
mgeovany <marlongeo1999@gmail.com>
2024-03-14 01:39:32 -0600

    feat: added principal colors

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,31 @@
-import $ from 'dom7';
-import Framework7 from './framework7-custom.js';
+import $ from "dom7";
+import Framework7 from "./framework7-custom.js";
 
 // Import F7 Styles
-import '../css/framework7-custom.less';
+import "../css/framework7-custom.less";
 
 // Import Icons and App Custom Styles
-import '../css/icons.css';
-import '../css/app.less';
-
+import "../css/icons.css";
+import "../css/app.less";
 
 // Import Routes
-import routes from './routes.js';
+import routes from "./routes.js";
 // Import Store
-import store from './store.js';
+import store from "./store.js";
 
 // Import main app component
-import App from '../app.f7';
-
+import App from "../app.f7";
 
 var app = new Framework7({
-  name: 'Rare Beauty App', // App name
-  theme: 'auto', // Automatic theme detection
+  name: "Rare Beauty App", // App name
+  theme: "auto", // Automatic theme detection
   colors: {
-    primary: '#f2e2d3',
+    primary: "#863356",
+    secondary: "#f2e2d3",
+    tertiary: "#e6c5b3",
   },
-  darkMode: true,
-  el: '#app', // App root element
+  darkMode: false,
+  el: "#app", // App root element
   component: App, // App main component
   // App store
   store: store,
